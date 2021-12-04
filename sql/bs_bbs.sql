30f0643916b3e2aeb65c3d56d84fd6e010f92aad
ddherm <92216280+ddherm@users.noreply.github.com>
2021-12-04 21:14:32 +0800

    Update bs_bbs.sql

--- sql/bs_bbs.sql
+++ sql/bs_bbs.sql
@@ -50,22 +50,90 @@ SET time_zone = "+00:00";
 
 --
 -- Table structure for table `friends`
+--CREATE TABLE `friends` (
+  `id` int(11) NOT NULL COMMENT '编号',
+  `f_user` int(11) DEFAULT NULL COMMENT '发起人',
+  `t_user` int(11) DEFAULT NULL COMMENT '目标人',
+  `conf` int(11) DEFAULT NULL COMMENT '是否确认'
+) ENGINE=MyISAM DEFAULT CHARSET=gb2312 COMMENT='好友';
+
 --
+-- Dumping data for table `friends`
+--
+
+INSERT INTO `friends` (`id`, `f_user`, `t_user`, `conf`) VALUES
+(3, 11, 10, 0);
+
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `msg`
+--CREATE TABLE `msg` (
+  `id` int(11) NOT NULL COMMENT '编号',
+  `f_user` int(11) DEFAULT NULL COMMENT '发起人',
+  `t_user` int(11) DEFAULT NULL COMMENT '目标人',
+  `conts` text COMMENT '内容',
+  `post_time` datetime DEFAULT NULL COMMENT '时间'
+) ENGINE=MyISAM DEFAULT CHARSET=gb2312 COMMENT='短消息';
+
 --
+-- Dumping data for table `msg`
+--
+
+INSERT INTO `msg` (`id`, `f_user`, `t_user`, `conts`, `post_time`) VALUES
+(3, 11, 10, '发消息发消息发消息发消息发消息发消息发消息', '2021-11-30 13:57:44');
+
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `posts`
+--CREATE TABLE `posts` (
+  `id` int(11) NOT NULL COMMENT '编号',
+  `title` varchar(50) DEFAULT NULL COMMENT '标题',
+  `conts` text COMMENT '内容',
+  `post_time` datetime DEFAULT NULL COMMENT '时间',
+  `user_id` int(11) DEFAULT NULL COMMENT '会员',
+  `cat_id` int(11) DEFAULT NULL COMMENT '栏目',
+  `status` int(11) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=gb2312 COMMENT='帖子';
+
 --
+-- Dumping data for table `posts`
+--
+
+INSERT INTO `posts` (`id`, `title`, `conts`, `post_time`, `user_id`, `cat_id`, `status`) VALUES
+(16, '测试测试测试测试', '测试测试测试测试测试测试测试测试测试测试', '2021-11-30 13:53:34', 10, 6, 1),
+(17, '测试测试测试测试', '<p>\r\n	测试测试测试测试\r\n</p>\r\n<p>\r\n	<a class=\"ke-insertfile\" href=\"/upfile/editor/file/20211130/20211130135346_89431.docx\" target=\"_blank\">试卷资料</a>\r\n</p>', '2021-11-30 13:54:02', 10, 7, 1),
+(18, '测试测试测试测试测试测试测试测试', '测试测试测试测试测试测试', '2021-11-30 13:54:11', 10, 8, 1),
+(15, '测试测试测试测试', '测试测试测试测试', '2021-11-30 13:53:19', 10, 5, 1),
+(14, '测试1', '测试1', '2021-11-30 13:53:04', 10, 5, 1);
+
+
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `users`
 --
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL COMMENT '编号',
+  `passport` varchar(50) DEFAULT NULL COMMENT '帐号',
+  `password` varchar(50) DEFAULT NULL COMMENT '密码',
+  `sex` varchar(10) DEFAULT NULL COMMENT '性别',
+  `birthday` varchar(200) DEFAULT NULL COMMENT '生日',
+  `qq` varchar(20) DEFAULT NULL COMMENT 'qq',
+  `addr` varchar(100) DEFAULT NULL COMMENT '地址',
+  `reg_time` datetime DEFAULT NULL COMMENT '注册时间',
+  `avatar` varchar(200) NOT NULL COMMENT '头像'
+) ENGINE=MyISAM DEFAULT CHARSET=gb2312 COMMENT='会员';
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `passport`, `password`, `sex`, `birthday`, `qq`, `addr`, `reg_time`, `avatar`) VALUES
+(11, '测试2', '111', '男', '2021-11-07', '913464179', '广州天河', '2021-11-30 13:55:39', 'upfile/images/20211130135519.jpg'),
+(10, '测试1', '111', '男', '2021-11-01', '913464179', '广州天河区', '2021-11-30 13:52:44', 'upfile/images/20211130135216.png');
+
 
 --
 -- Indexes for dumped tables
